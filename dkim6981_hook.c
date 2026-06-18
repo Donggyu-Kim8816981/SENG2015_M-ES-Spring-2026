a4796458d2787beae89038612f82d1fb8d140b68
Donggyu Kim <dkim6981@conestogac.on.ca>
2026-06-18 19:19:40 -0400

    A02-implemented the logic for input handling in C

--- dkim6981_hook.c
+++ dkim6981_hook.c
@@ -71,3 +71,50 @@ void dkim6981_StringTest(int action)
 }
 
 ADD_CMD("dkim6981_string", dkim6981_StringTest,"Test the new string function")
+
+// Assignment 2 Prototype
+int dkim6981_a2(int num, int wait);
+
+// Assignment 2 C Function
+void _dkim6981_Assignment2(int action)
+{
+
+  if(action==CMD_SHORT_HELP) return;
+  if(action==CMD_LONG_HELP) {
+    printf("Assignment 2\n\n"
+	   "This command triggers assignment 2 by dkim6981\n"
+	   );
+
+    return;
+  }
+
+  // Retrieve user inputs for count and delay here
+  uint32_t user_input;
+  int fetch_status;
+
+  uint32_t num; // Stores how many times the full LED toggle cycle will repeat.
+
+  fetch_status = fetch_uint32_arg(&user_input); // Read the first argument from minicom as user input.
+
+  if(fetch_status) {
+    num = 1; // Default cycle count. One full cycle toggles all 8 LEDs once.
+  } 
+  else {
+    num = user_input;
+  }
+
+  uint32_t wait; // Stores the delay value between each LED toggle.
+
+  fetch_status = fetch_uint32_arg(&user_input); // Read the second argument from minicom as user input.
+
+  if(fetch_status){
+    wait = 0xFFFFEF; // Default delay value between each LED toggle.
+  }
+  else {
+    wait = user_input;
+  }
+
+  printf("dkim6981_a2 returned: %d\n", dkim6981_a2 ((int)num, (int)wait) );
+}
+
+ADD_CMD("dkim6981_a2", _dkim6981_Assignment2, "Assignment 2")
